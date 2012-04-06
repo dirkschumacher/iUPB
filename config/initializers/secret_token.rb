df6f6b608af54d11743fea609bd69415a4bfdb77
Dirk Schumacher <mail@dirk-schumacher.net>
2012-04-06 18:58:47 +0200

    sets up the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IUPB::Application.config.secret_token = '1aaacaff323b8e9872335452a4283eff866e50c5aa21412c7d9597306240c0412104cfcc82e9dfd94c0f58e73c73a550b4253d38922bc90bc6b18779c302922d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IUPB::Application.config.session_store :cookie_store, key: '_iUPB_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IUPB::Application.config.session_store :active_record_store
