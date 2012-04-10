95c12f8814b6ddf7c0ed6b505a925f4424c2d6ad
Dirk Schumacher <mail@dirk-schumacher.net>
2012-04-10 23:08:05 +0200

    fixes a spelling error in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Restaurant.destory_all
+Restaurant.delete_all
 Restaurant.create([
   { name: 'Mensa', feed_url:'http://www.studentenwerk-pb.de/fileadmin/xml/mensa.xml' }, 
   { name: 'Gownsmen Pub', feed_url:'http://www.studentenwerk-pb.de/fileadmin/xml/gownsmenspub.xml'  }, 
