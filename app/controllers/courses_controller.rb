da5417e39fe01081a7113bbd500fe1a9b8a8de66
Dirk Schumacher <mail@dirk-schumacher.net>
2012-04-19 18:52:42 +0200

    limits the courses search api to 10 results

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -23,7 +23,7 @@ class CoursesController < ApplicationController
   
   def search
     query = params[:query].downcase
-    @courses = Course.where(title_downcase: /.*#{query}.*/).all
+    @courses = Course.where(title_downcase: /.*#{query}.*/).limit(10).all
     # If the request is stale according to the given timestamp and etag value
     # (i.e. it needs to be processed again) then execute this block
     if stale?(:last_modified => Time.now - 7.days, :etag => @courses)
