e91c3a812159a50078c24c77da7cf96873a68347
Dirk Schumacher <mail@dirk-schumacher.net>
2012-05-08 21:33:56 +0200

    pulls bus information directly from the api and makes minor changes to the language files

--- app/controllers/transportation_controller.rb
+++ app/controllers/transportation_controller.rb
@@ -1,18 +1,4 @@
 class TransportationController < ApplicationController
   def index
-    @trips = get_transportation_data
-  end
-  
-  protected
-  def get_transportation_data
-    # naive approach, not for production
-    data = Rails.cache.fetch('transportation_json_data', :expires_in => 2.minute) do
-      result = JSON.parse(open("http://upbapi.cloudcontrolled.com/busplan.php").read)
-      result.map do |trip| # mapping to make a DateTime object out of the "da" attribute. Can then be localized in view.
-        trip["date"] = DateTime::strptime(trip["date"], "%Y-%m-%d %H:%M:%S")
-        trip
-      end
-    end
-    data
   end
 end
