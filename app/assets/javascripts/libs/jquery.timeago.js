1ed2880d098d4dd96ff7bbe3c0c4c86987ec84d8
Dirk Schumacher <mail@dirk-schumacher.net>
2012-05-19 14:39:06 +0200

    refactors the twitter js code. Adds translations to time ago plugin and modifies the twitter layout

--- app/assets/javascripts/libs/jquery.timeago.js
+++ app/assets/javascripts/libs/jquery.timeago.js
@@ -30,35 +30,16 @@
       refreshMillis: 60000,
       allowFuture: false,
       strings: {
-        prefixAgo: null,
-        prefixFromNow: null,
-        suffixAgo: "ago",
-        suffixFromNow: "from now",
-        seconds: "less than a minute",
-        minute: "about a minute",
-        minutes: "%d minutes",
-        hour: "about an hour",
-        hours: "about %d hours",
-        day: "a day",
-        days: "%d days",
-        month: "about a month",
-        months: "%d months",
-        year: "about a year",
-        years: "%d years",
-        wordSeparator: " ",
-        numbers: []
+        seconds: "jquery.timeago.seconds",
+        minutes: "jquery.timeago.minutes",
+        hours: "jquery.timeago.hours",
+        days: "jquery.timeago.days",
+        months: "jquery.timeago.months",
+        years: "jquery.timeago.years"
       }
     },
     inWords: function(distanceMillis) {
       var $l = this.settings.strings;
-      var prefix = $l.prefixAgo;
-      var suffix = $l.suffixAgo;
-      if (this.settings.allowFuture) {
-        if (distanceMillis < 0) {
-          prefix = $l.prefixFromNow;
-          suffix = $l.suffixFromNow;
-        }
-      }
 
       var seconds = Math.abs(distanceMillis) / 1000;
       var minutes = seconds / 60;
@@ -66,26 +47,19 @@
       var days = hours / 24;
       var years = days / 365;
 
-      function substitute(stringOrFunction, number) {
-        var string = $.isFunction(stringOrFunction) ? stringOrFunction(number, distanceMillis) : stringOrFunction;
-        var value = ($l.numbers && $l.numbers[number]) || number;
-        return string.replace(/%d/i, value);
-      }
+      var words = seconds < 45 && I18n.t($l.seconds, {count: Math.round(seconds)}) ||
+        seconds < 90 && I18n.t($l.minutes, {count: 1}) ||
+        minutes < 45 && I18n.t($l.minutes, {count: Math.round(minutes)}) ||
+        minutes < 90 && I18n.t($l.hours, {count: 1})||
+        hours < 24 && I18n.t($l.hours, {count: Math.round(hours)}) ||
+        hours < 42 && I18n.t($l.days, {count: 1}) ||
+        days < 30 && I18n.t($l.days, {count: Math.round(days)}) ||
+        days < 45 && I18n.t($l.months, {count: 1}) ||
+        days < 365 && I18n.t($l.months, {count: Math.round(days / 30)}) ||
+        years < 1.5 && I18n.t($l.years, {count: 1}) ||
+        I18n.t($l.years, {count: Math.round(years)});
 
-      var words = seconds < 45 && substitute($l.seconds, Math.round(seconds)) ||
-        seconds < 90 && substitute($l.minute, 1) ||
-        minutes < 45 && substitute($l.minutes, Math.round(minutes)) ||
-        minutes < 90 && substitute($l.hour, 1) ||
-        hours < 24 && substitute($l.hours, Math.round(hours)) ||
-        hours < 42 && substitute($l.day, 1) ||
-        days < 30 && substitute($l.days, Math.round(days)) ||
-        days < 45 && substitute($l.month, 1) ||
-        days < 365 && substitute($l.months, Math.round(days / 30)) ||
-        years < 1.5 && substitute($l.year, 1) ||
-        substitute($l.years, Math.round(years));
-
-      var separator = $l.wordSeparator === undefined ?  " " : $l.wordSeparator;
-      return $.trim([prefix, words, suffix].join(separator));
+      return $.trim(words);
     },
     parse: function(iso8601) {
       var s = $.trim(iso8601);
--- app/assets/javascripts/twitter.js.coffee
+++ app/assets/javascripts/twitter.js.coffee
@@ -1,3 +1,51 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+class @Twitter
+  constructor: (@element) ->
+  parseUsername: (text) ->
+    text.replace /[@]+[A-Za-z0-9-_]+/g, (u) ->
+      u.link 'https://twitter.com/' + user.replace('@','')
+  
+  parseHashtag: (text) ->
+    text.replace /[#]+[A-Za-z0-9-_]+/g, (t) ->
+      tag = t.replace '#','%23'
+      t.link 'https://search.twitter.com/search?q=' + tag
+  
+  profileLink: (userid) ->
+    'https://twitter.com/#!/' + userid
+      
+  displayFeed: (post_event) ->
+      self = this
+      $.ajax({ 
+        url: "https://search.twitter.com/search.json?q=%23upb&lang=de&callback=?",
+        dataType: "jsonp",
+        type: "GET",
+        jsonp: true,
+        cache: true,
+        jsonpCallback: "success", 
+        success: (data) ->
+          self.element.empty()
+          tweets = data.results
+          for tweet in tweets
+            item = $ '<li>'
+            avatar = $ '<a class="twitter_avatar">'
+            avatar.attr('href', self.profileLink tweet.from_user)
+            user_image = $ '<img>'
+            user_image.attr('src', tweet.profile_image_url_https)
+            user_image.addClass 'box_round'
+            avatar.append user_image
+            textWrap = $ '<h4>'
+            detailsWrap = $ '<h6>'
+            textWrap.html self.parseUsername(self.parseHashtag(self.parseURL(tweet.text)))
+            date = Date.parse tweet.created_at
+            detailsWrap.html $.timeago(tweet.created_at) + ' ' + I18n.t('pages.twitter.by') + ' <a href="' +  self.profileLink tweet.from_user + '" target="_blank">' + tweet.from_user + '</a>'
+            messageContainer = $ '<div class="twitter_message well">'
+            item.append avatar
+            messageContainer.append textWrap
+            messageContainer.append detailsWrap
+            item.append messageContainer
+            self.element.append item
+          post_event()
+        })
+        
+   parseURL: (text)->
+     text.replace /[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, (url) ->
+       url.link url
