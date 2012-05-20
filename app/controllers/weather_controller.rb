2881a59b1c8610ffbaccfc0851e44c8f7c22617e
Michael Whittaker <post@michael-whittaker.de>
2012-05-20 14:15:30 +0200

    adds time zone; fixes whether nil error at midnight

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,7 +2,14 @@ class WeatherController < ApplicationController
   def index
     set_cache_header 60*20
     @weatherData = get_weather_data
-    @forecast = get_forecast
+    forecast = get_forecast
+    if forecast['city']['forecast'][Time.now.strftime("%Y-%m-%d")]
+      @forecast_temp = forecast['city']['forecast'][Time.now.strftime("%Y-%m-%d")]['tx']
+      @forecast_text = forecast['city']['forecast'][Time.now.strftime("%Y-%m-%d")]['w_txt']
+    else
+      @forecast_temp = nil
+      @forecast_text = nil
+    end
   end
   
   protected
@@ -16,11 +23,13 @@ class WeatherController < ApplicationController
   end
    
   def get_forecast
-    project = "iupb"
-    api_key = "6be11e5256109145398267ed1e953102"
-    citycode = "DE0008110"
-    checksum = Digest::MD5.hexdigest(project+api_key+citycode)
-    url = "/forecast/weather/city/"+citycode+"/project/"+project+"/cs/"+checksum+"/output/json"
-    ::Yajl::Parser.parse(open("http://api.wetter.com"+url))
+    Rails.cache.fetch("iUPB.wettercom_api", :expires_in => 1.hour) do
+      project = "iupb"
+      api_key = "6be11e5256109145398267ed1e953102"
+      citycode = "DE0008110"
+      checksum = Digest::MD5.hexdigest(project+api_key+citycode)
+      url = "/forecast/weather/city/"+citycode+"/project/"+project+"/cs/"+checksum+"/output/json"
+      ::Yajl::Parser.parse(open("http://api.wetter.com"+url))
+    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ IUPB::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.time_zone = 'Berlin'
 end
 require 'newrelic_rpm'
 def pp
