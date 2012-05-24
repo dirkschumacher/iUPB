2c9280e43a00337c1076014abc461dae0947beb6
Michael Whittaker <post@michael-whittaker.de>
2012-05-24 23:19:35 +0200

    fix fb config

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,5 +1,6 @@
 module Facebook
-  CONFIG = YAML.load_file(Rails.root.join("config/facebook.yml"))[Rails.env]
+  
+  CONFIG = YAML.load(ERB.new(File.new(Rails.root.join("config/facebook.yml")).read).result)[Rails.env]
   APP_ID = CONFIG['app_id']
   SECRET = CONFIG['secret_key']
   TOKEN = CONFIG['token']
