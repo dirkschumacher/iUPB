b2b7585908e167ed598820d9ed0b39c5da87f096
Dirk Schumacher <mail@dirk-schumacher.net>
2012-05-28 17:28:14 +0200

    caches static assets for seven days now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ IUPB::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=2592000"
+  config.static_cache_control = "public, max-age=604800"
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
