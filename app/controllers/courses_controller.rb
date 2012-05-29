a1051b13e811315241d9a974f772a8d7438c2392
Dirk Schumacher <mail@dirk-schumacher.net>
2012-05-29 22:47:06 +0200

    resolves https://www.pivotaltracker.com/story/show/30317417

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,10 +6,10 @@ class CoursesController < ApplicationController
   
   def show
     @course = Course.find(params[:id])
-    set_next_class @course
+    update_course @course
     if @course.course_type == 'course'
       @groups = Course.where(paul_id: @course.paul_id).where(course_type: 'group').excludes(id: @course.id).order_by([[:title_downcase, :asc]]).entries
-      attach_next_class @groups
+      update_courses @groups
     end
     # If the request is stale according to the given timestamp and etag value
     # (i.e. it needs to be processed again) then execute this block
@@ -23,10 +23,10 @@ class CoursesController < ApplicationController
     @courses = []
     @courses = Course.where(course_type: 'course').where(title_downcase: /.*#{query}.*/).limit(10).entries if query.length > 2
     
-    attach_next_class @courses
+    update_courses @courses
     set_cache_header(60*60) # only cache 1 hour
   end
-  def set_next_class(course)
+  def update_course(course)
     min_interval = 100.days
     course.course_data.each do |data|
       next_class = data['date'].to_date
@@ -36,8 +36,8 @@ class CoursesController < ApplicationController
       time_to = Time.new(next_class.year, next_class.mon, next_class.day, data['time_to'].hour, data['time_to'].min, 0, data['time_to'].utc_offset).in_time_zone(Time.zone)
       data['time_from'] = time_from
       data['time_to'] = time_to
-      interval = time_from - Time.now
-      if time_from >= Time.now and interval < min_interval
+      interval = time_to - Time.now
+      if time_to >= Time.now and interval < min_interval
         course['next_class'] = {
           room: data['room'].length == 0 ? t('courses.room_na') : data['room'],
           time_from: time_from,
@@ -48,9 +48,9 @@ class CoursesController < ApplicationController
       end
     end
   end
-  def attach_next_class(courses)
+  def update_courses(courses)
     courses.each do |course|
-      set_next_class course
+      update_course course
     end
   end
 end
