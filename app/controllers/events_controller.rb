f530a443016925f16d86a786186b0bf6d1af88d1
Michael Whittaker <post@michael-whittaker.de>
2012-07-05 14:35:30 +0200

    fixes facebook Bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,11 +7,26 @@ class EventsController < ApplicationController
 
       # 135017669880336 is https://www.facebook.com/uniparty.pb
      events = graph.get_connections("135017669880336", "events").map do |event|
-        event["start_time"] = DateTime::strptime(event["start_time"]+"T+0200", "%Y-%m-%dT%H:%M:%ST%z")
-        event["end_time"] = DateTime::strptime(event["end_time"]+"T+0200", "%Y-%m-%dT%H:%M:%ST%z")
-        event["link"] = "https://www.facebook.com/events/" + event["id"]
-        event["attendees"] = graph.get_connections(event["id"], "attending").count if event["start_time"] > DateTime.now
-        event
+       begin
+         unless event["start_time"].include?("+")
+           event["start_time"] =DateTime::strptime(event["start_time"]+"T+0200", "%Y-%m-%dT%H:%M:%ST%z")
+           if event["end_time"]
+             event["end_time"] = DateTime::strptime(event["end_time"]+"T+0200", "%Y-%m-%dT%H:%M:%ST%z")
+          else
+            event["end_time"] = event["start_time"]
+          end
+         else
+           event["start_time"] = DateTime::strptime(event["start_time"].to_s, "%Y-%m-%dT%H:%M:%S%z")
+           if event["end_time"]
+              event["end_time"] = DateTime::strptime(event["end_time"].to_s, "%Y-%m-%dT%H:%M:%S%z")
+           else
+             event["end_time"] = event["start_time"]
+           end
+         end
+       end
+       event["link"] = "https://www.facebook.com/events/" + event["id"]
+       event["attendees"] = graph.get_connections(event["id"], "attending").count if event["start_time"] > DateTime.now
+       event
       end
       events.sort! { |l, r| l["start_time"]<=>r["start_time"] } # necessary?
     #  @events = events
