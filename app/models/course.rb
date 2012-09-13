9be959dbb851d5f52b5a487e076fb1a21ebaa837
Michael Whittaker <post@michael-whittaker.de>
2012-09-14 01:27:52 +0200

    prepares 40 years view for facts and small improvements adding courses to the timetable

--- app/models/course.rb
+++ app/models/course.rb
@@ -20,7 +20,6 @@ class Course
   def get_dates(future=true)
     dates = []
     self.course_data.each do |c|
-      puts c["date"].year, c["date"].month, c["date"].day, c["time_from"].to_time.hour, c["time_from"].to_time.min
       start_time = Time.mktime(c["date"].year, c["date"].month, c["date"].day, c["time_from"].to_time.hour, c["time_from"].to_time.min)   #mktime(year, month, day, hour, min)
       end_time = Time.mktime(c["date"].year, c["date"].month, c["date"].day, c["time_to"].to_time.hour, c["time_to"].to_time.min)
       dates << [start_time, end_time, c["room"], c["instructor"]] unless future && start_time < Time.now
