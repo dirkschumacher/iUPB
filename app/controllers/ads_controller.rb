09e4beeb9d68e736f3eb1b606f2dafaa4b7e8dc9
Dirk Schumacher <mail@dirk-schumacher.net>
2013-01-12 15:33:15 +0100

    adds a form for ads

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -5,10 +5,20 @@ class AdsController < ApplicationController
   def new
     @ad = Ad.new
   end
-
+  
   def edit
   end
 
   def remove
+  end
+
+  def create
+    @ad = Ad.new(params[:ad])
+    @ad.user = current_user if user_signed_in?
+    if @ad.save
+      redirect_to ads_path, notice: t(".notice_saved")
+    else
+      render "new"
+    end
   end
 end
