842daa092208918cee5458fadde0be7525953994
Dirk Schumacher <mail@dirk-schumacher.net>
2013-03-10 13:22:10 +0100

    refactors the omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,27 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # You need to implement the method below in your model
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    try_login_user "Facebook"
   end
 
   def google_oauth2
-      @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-
-      if @user.persisted?
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in_and_redirect @user, :event => :authentication
-      else
-        session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+    try_login_user "Google"
   end
-
+  
+  private 
+  
+  def try_login_user(kind)
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => kind
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise." + kind.downcase + "_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 end
