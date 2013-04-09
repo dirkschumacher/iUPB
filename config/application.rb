48dde1df5b41d574258a2a955fe08817043f9e1d
Michael Whittaker <post@michael-whittaker.de>
2013-04-09 02:22:37 +0200

    try with  heroku labs:enable user-env-compile. Reverts "try to fix a heroku/tire issue, https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting"
    This reverts commit c4fa61e7b2e350c678bd96ba3da2937ec97bbeda.

--- config/application.rb
+++ config/application.rb
@@ -58,9 +58,6 @@ module IUPB
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    
-    # https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
-    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
