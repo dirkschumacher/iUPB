20b3ddd12bc101391510ef32a70c38ff36b5164f
Michael Whittaker <post@michael-whittaker.de>
2013-11-20 16:52:43 +0100

    disables Facebook party query

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,9 +2,9 @@ class EventsController < ApplicationController
   before_filter :set_cache_header
   
   def index
-    @events = Rails.cache.fetch("iUPB.fb_parties", :expires_in => 2.hours) do
-      FacebookHelper::facebook_events("135017669880336") # 135017669880336 is https://www.facebook.com/uniparty.pb
-    end
+    #@events = Rails.cache.fetch("iUPB.fb_parties", :expires_in => 2.hours) do
+    #  FacebookHelper::facebook_events("135017669880336") # 135017669880336 is https://www.facebook.com/uniparty.pb
+    #end
     @parties = Party.all
   end
 end
