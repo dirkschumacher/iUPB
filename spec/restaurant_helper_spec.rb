8fb1b07ffed9f6b731c130a4e2135d5275148545
Dirk Schumacher <mail@dirk-schumacher.net>
2014-01-26 21:29:43 +0100

    removes the test folder and reformats spec

--- spec/restaurant_helper_spec.rb
+++ spec/restaurant_helper_spec.rb
@@ -2,11 +2,10 @@ require "spec_helper"
 
 describe RestaurantHelper, "#get_menu_data" do
   it "ignores mensa hamm" do
-  	file_name = "./spec/fixtures/example_stwb_input.csv"
+	file_name = "./spec/fixtures/example_stwb_input.csv"
     restaurant = double("restaurant")
     restaurant.stub(:menus).and_return(double("menus", :where => [false]))
     Restaurant.stub(:where).and_return([restaurant]) 
-    
     parsed_data = RestaurantHelper.get_menu_data(file_name)
     parsed_data.length.should equal 3
     end
